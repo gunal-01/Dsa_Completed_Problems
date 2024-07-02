3f6bb4357bd02bcf218d05148bda720c485fb7bb
gunal-01 <gunalprakash006@gmail.com>
2024-07-02 15:27:42 +0530

    new commit

---
+++ Palindrome.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var y = x.toString().split('').reverse().join('');
+    if(y == x) {
+        return true;
+    } else {
+        return false;
+    }
+};
