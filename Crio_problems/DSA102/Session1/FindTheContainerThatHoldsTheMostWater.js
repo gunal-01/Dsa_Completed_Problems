2004d576b9c26bab4855d303409efe5471c80b4c
gunal-01 <gunalprakash006@gmail.com>
2024-08-04 04:35:51 +0530

    smallErrorCorrection

--- Crio_problems/DSA102/Session1/FindTheContainerThatHoldsTheMostWater.js
+++ Crio_problems/DSA102/Session1/FindTheContainerThatHoldsTheMostWater.js
@@ -2,7 +2,7 @@ function maxArea(height){
     let left = 0, right = height.length-1;
     let maxArea = 0;
     while(left < right) {
-        let currentArea = Math.max(height[left], height[right])*(right-left);
+        let currentArea = Math.min(height[left], height[right])*(right-left);
         maxArea = Math.max(maxArea, currentArea);
         if(height[left] < height[right]) {
             left++;
