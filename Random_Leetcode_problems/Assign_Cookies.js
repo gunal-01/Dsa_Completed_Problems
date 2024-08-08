8d2ae31f291cde264d1133aef10273f12c3cd37c
gunal-01 <gunalprakash006@gmail.com>
2024-08-09 00:04:11 +0530

    assign_cookiesProblem

---
+++ Random_Leetcode_problems/Assign_Cookies.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    let g1 = g.length;
+    let s1 = s.length;
+    let children = 0;
+    g.sort((a,b) => a - b)
+    s.sort((a,b) => a - b)
+    let gg = 0, ss = 0;
+    while(gg < g1 && ss < s1) {
+        if(s[ss] >= g[gg]) {
+            children++;
+            gg++
+        }
+        ss++
+    }
+    return children;
+};
+
+//two pointer practice problem
