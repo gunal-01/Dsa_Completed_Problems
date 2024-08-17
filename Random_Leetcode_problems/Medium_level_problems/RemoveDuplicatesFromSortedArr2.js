5b5ac7a8941edc29e4300a2d5a4086f48094a7dc
gunal-01 <gunalprakash006@gmail.com>
2024-08-17 22:10:10 +0530

    doneWithTwoPointerApproach

---
+++ Random_Leetcode_problems/Medium_level_problems/RemoveDuplicatesFromSortedArr2.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if(!nums.length) return 0;
+    let k = 0;
+
+    for(let i=0; i < nums.length; i++) {
+        if(k < 2 || nums[i] > nums[k - 2]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
