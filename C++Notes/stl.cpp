efa9f69837643e109521e5c74a77ac7dc2fa33df
gunal-01 <gunalprakash006@gmail.com>
2024-11-07 20:41:16 +0530

    stl-notes

---
+++ C++Notes/stl.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void explainPair() {
+    
+    pair<int, int> p = {1,3};
+
+    cout << p.first << " " << p.second;
+    
+    
+    pair<int, pair<int, int>> newPair = {1,{3,4}};
+    
+    cout << newPair.first << " " << newPair.second.second << " " << newPair.second.first;
+    
+    pair<int, int> arr[] = {{1,2}, {2,5}, {5,1}};
+    
+    cout << arr[1].second;
+}
+
+int main() {
+    explainPair();
+}
