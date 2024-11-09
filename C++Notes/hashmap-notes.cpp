64db74348deac506b5580dd9ced56679748cc3df
gunal-01 <gunalprakash006@gmail.com>
2024-11-09 18:10:41 +0530

    hashmap-notes updated

---
+++ C++Notes/hashmap-notes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Hashmap notes:
+
+/*int main() {
+    unordered_map<string, int> myMap;
+    
+    // inserting elments
+    myMap["apple"] = 3;
+    myMap["banana"] = 2;
+    myMap["orange"] = 1;
+    
+    // Iterating over elements
+    for(const pair<const string, int>& x : myMap) {
+        cout<< x.first << " " << x.second << endl;
+    }
+    return 0;
+}*/
